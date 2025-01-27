aa6fa4cff3d3f1c95f1934617f1adc4a8b034eeb
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-28 00:41:53 +0100

    styles: button
    Quit game and start game

--- src/components/game/Wizzard.tsx
+++ src/components/game/Wizzard.tsx
@@ -11,16 +11,22 @@ function Wizzard() {
 
 	return (
 		<div className='w-full space-y-2 h-full flex flex-col'>
-			<h2 className='font-semibold  opacity-80 tracking-wider text-3xl md:text-4xl'>
+			<h2 className='opacity-80 tracking-wider text-3xl md:text-4xl'>
 				Game preferences
 			</h2>
-			<section className='flex-1 p-2 flex flex-col justify-start  md:items-center w-full space-y-4  pt-4 rounded-md'>
+			<section className='flex-1 p-2 flex flex-col md:flex-row justify-start  md:items-start w-full space-y-4  pt-4 rounded-md'>
 				<CategoryPicker />
 				<Difficultypicker />
 			</section>
-			<section className='w-full p-2 flex justify-center items-center'>
+			<section className='w-full p-2 flex flex-col md:flex-row justify-center gap-2 items-center'>
 				<button
-					className='font-normal select-none tracking-tight w-full md:text-xl lg:max-w-xl text-white/60 hover:text-white text-lg px-4 py-3 rounded-md bg-white/5 transition-all hover:shadow-xl hover:scale-105 border-none cursor-pointer hover:border-white'
+					className='font-normal bg-emerald-400/50 hover:bg-emerald-400 text-green-300  focus:bg-emerald-400 select-none tracking-tight w-full md:text-xl lg:max-w-xl  hover:text-green-800 text-lg px-4 py-3 rounded-md  hover:font-medium transition-all hover:shadow-xl hover:scale-102 border-none cursor-pointer hover:border-white'
+					onClick={handleQuitGame}
+				>
+					Start game
+				</button>
+				<button
+					className='font-normal select-none bg-rose-400/50 hover:bg-rose-400 tracking-tight w-full md:text-xl lg:max-w-xl text-red-300 hover:text-red-800 hover:font-medium focus:bg-rose-400 text-lg px-4 py-3 rounded-md  transition-all hover:shadow-xl hover:scale-102 border-none cursor-pointer hover:border-white'
 					onClick={handleQuitGame}
 				>
 					Quit game
