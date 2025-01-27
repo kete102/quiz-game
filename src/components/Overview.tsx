b27aa7528153c8da8658f2d9efc2616ec7d1db5b
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-28 00:54:44 +0100

    feat: PlayerStats component

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,4 +1,6 @@
+import { ArrowRight } from 'lucide-react'
 import { useGameStore } from '../store/game/store'
+import PlayerStats from './game/PlayerStats'
 
 function Overview() {
 	const { setIsGameActive } = useGameStore()
@@ -6,16 +8,24 @@ function Overview() {
 		setIsGameActive(true)
 	}
 	return (
-		<div className='w-full h-full grid place-content-center'>
-			<section>
-				<h1>Last games</h1>
+		<div className='w-full h-full space-y-4'>
+			<section className='w-full flex items-center justify-between'>
+				<h1 className='opacity-80 tracking-wider text-3xl md:text-4xl'>
+					Overview
+				</h1>
+				<button
+					className='font-normal hover:font-medium inline-flex items-center gap-x-1.5 select-none tracking-tight text-white/60 hover:text-white text-xl px-4 py-3 rounded-md bg-white/5 transition-all hover:shadow-xl hover:scale-105 border-none cursor-pointer hover:border-white'
+					onClick={handleStartGame}
+				>
+					Start a new game! <ArrowRight />
+				</button>
 			</section>
-			<button
-				className='font-normal select-none tracking-tight text-white/60 hover:text-white text-3xl px-4 py-3 rounded-md bg-white/5 transition-all hover:shadow-xl hover:scale-105 border-none cursor-pointer hover:border-white'
-				onClick={handleStartGame}
-			>
-				Start a new game!
-			</button>
+			<section className='grid gap-4 grid-cols-1 md:grid-cols-2 md:grid-rows-2 p-2'>
+				<PlayerStats />
+				<PlayerStats />
+				<PlayerStats />
+				<PlayerStats />
+			</section>
 		</div>
 	)
 }
---
+++ src/components/game/PlayerStats.tsx
@@ -0,0 +1,7 @@
+function PlayerStats() {
+	return (
+		<article className='h-72 rounded-md w-full shadow-2xl border-[1px] border-white/30'></article>
+	)
+}
+
+export default PlayerStats
