17f6169ac4e553a38e77d84d91a177098f98407f
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-28 00:19:18 +0100

    feat: categoryPicker, difficultyPicker

---
+++ src/components/game/CategoryPicker.tsx
@@ -0,0 +1,25 @@
+import { Plus } from 'lucide-react'
+import { CATEGORIES } from '../../utils/constants'
+
+function CategoryPicker() {
+	return (
+		<section className='p-2 w-full'>
+			<h2 className='text-2xl opacity-60'>Categories</h2>
+			<div className='flex mt-4 items-center flex-wrap gap-2 w-full max-w-xl'>
+				{CATEGORIES.map((category) => (
+					<button
+						key={category.id}
+						className='text-[1rem] active:border-green-800 inline-flex items-center gap-x-2 cursor-pointer border-2 border-white/50 p-2 rounded-md'
+					>
+						<span className='flex items-center gap-x-1'>
+							{category.name} {category.icon}
+						</span>
+						<Plus className='size-4' />
+					</button>
+				))}
+			</div>
+		</section>
+	)
+}
+
+export default CategoryPicker
---
+++ src/components/game/DifficultyPicker.tsx
@@ -0,0 +1,26 @@
+import { Check } from 'lucide-react'
+import { Difficulty } from '../../services/api/types'
+import { DIFFICULTIES } from '../../utils/constants'
+
+function Difficultypicker() {
+	return (
+		<section className='p-2 w-full'>
+			<h2 className='text-2xl opacity-60'>Difficulty</h2>
+			<div className='flex mt-4 items-center flex-wrap gap-2 w-full max-w-xl'>
+				{DIFFICULTIES.map((difficulty: Difficulty) => (
+					<button
+						key={difficulty.id}
+						className='text-[1rem] active:border-green-800 inline-flex items-center gap-x-2 cursor-pointer border-2 border-white/50 p-2 rounded-md'
+					>
+						<span className='flex items-center gap-x-1'>
+							{difficulty.name} {difficulty.icon}
+						</span>
+						<Check className='size-4' />
+					</button>
+				))}
+			</div>
+		</section>
+	)
+}
+
+export default Difficultypicker
