68d99441ae975464894506292b47897d8b317911
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-27 21:18:30 +0100

    styles: fix height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { SignedIn, UserButton } from '@clerk/clerk-react'
 
 function Header() {
 	return (
-		<header className='w-full  select-none mx-auto px-2 md:px-4 dark:bg-black/30 bg-white/50 mt-2 flex items-center justify-between p-4 rounded-lg'>
+		<header className='w-full min-h-20 select-none mx-auto px-2 md:px-4 dark:bg-black/30 bg-white/50 mt-2 flex items-center justify-between p-4 rounded-lg'>
 			<h1 className='text-2xl text-black dark:text-white font-bold tracking-wide'>
 				Quizz Game
 			</h1>
