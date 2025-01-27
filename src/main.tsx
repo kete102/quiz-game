b03e7d3632dd75abbeeb3c14a9b4c4674c185b87
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-27 13:29:26 +0100

    feat: auth
    Clerk authentication

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { clerkApiKey } from '../utils/constants.ts'
+
+if (!clerkApiKey) throw new Error('Clerk Error: Missing publishable key')
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<ClerkProvider publishableKey={clerkApiKey}>
+		<StrictMode>
+			<App />
+		</StrictMode>
+	</ClerkProvider>
 )
