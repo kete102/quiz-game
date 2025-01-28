da3c2ba090bfec85318375a0df7f1fa4a83fa10f
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-28 12:11:01 +0100

    styles: PlayerStats

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -7,11 +7,12 @@ function Overview() {
 	const handleStartGame = () => {
 		setIsGameActive(true)
 	}
+
 	return (
 		<div className='w-full h-full space-y-4'>
 			<section className='w-full flex items-center justify-between'>
 				<h1 className='opacity-80 tracking-wider text-3xl md:text-4xl'>
-					Overview
+					Hi, kete 👋
 				</h1>
 				<button
 					className='font-normal hover:font-medium inline-flex items-center gap-x-1.5 select-none tracking-tight text-white/60 hover:text-white text-xl px-4 py-3 rounded-md bg-white/5 transition-all hover:shadow-xl hover:scale-105 border-none cursor-pointer hover:border-white'
@@ -21,10 +22,34 @@ function Overview() {
 				</button>
 			</section>
 			<section className='grid gap-4 grid-cols-1 md:grid-cols-2 md:grid-rows-2 p-2'>
-				<PlayerStats />
-				<PlayerStats />
-				<PlayerStats />
-				<PlayerStats />
+				<PlayerStats>
+					<h3 className='opacity-50 text-3xl text-center'>100 games played</h3>
+					<div className='mt-8 flex items-center justify-center gap-x-4'>
+						<h4 className='text-green-400 text-2xl '>10 wins 🎉</h4>
+						<h4 className='text-rose-400 text-2xl'>7 losses 😨</h4>
+					</div>
+				</PlayerStats>
+				<PlayerStats>
+					<h3 className='opacity-50 text-3xl text-center'>Favorite topics</h3>
+					<div className='mt-8 w-full flex items-center'>
+						<h4 className='text-green-400'>10 wins</h4>
+						<h4 className='text-rose-400'>7 losses</h4>
+					</div>
+				</PlayerStats>
+				<PlayerStats>
+					<h3 className='opacity-50 text-3xl text-center'>Achivements</h3>
+					<div className='mt-8 w-full flex items-center'>
+						<h4 className='text-green-400'>10 wins</h4>
+						<h4 className='text-rose-400'>7 losses</h4>
+					</div>
+				</PlayerStats>
+				<PlayerStats>
+					<h3 className='opacity-50 text-3xl text-center'>Average score</h3>
+					<h4 className='text-center my-2'>7</h4>
+					<p className='italic opacity-30 text-xl font-normal'>
+						Correct answers increases this score
+					</p>
+				</PlayerStats>
 			</section>
 		</div>
 	)
--- src/components/game/PlayerStats.tsx
+++ src/components/game/PlayerStats.tsx
@@ -1,6 +1,10 @@
-function PlayerStats() {
+import { ReactNode } from 'react'
+
+function PlayerStats({ children }: { children: ReactNode }) {
 	return (
-		<article className='h-72 rounded-md w-full shadow-2xl border-[1px] border-white/30'></article>
+		<article className='min-h-52 max-h-fit rounded-md w-full grid place-content-center shadow-2xl border-[1px] border-white/30 p-2'>
+			{children}
+		</article>
 	)
 }
 
