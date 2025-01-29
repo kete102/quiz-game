258560f27c1ddce72f685e844a23bc640a5454b7
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-29 19:13:57 +0100

    feat: quit game
    Show quita game button only when user is signed in

--- src/components/game/Wizzard.tsx
+++ src/components/game/Wizzard.tsx
@@ -1,8 +1,10 @@
+import { useAuth } from '@clerk/clerk-react'
 import { useGameStore } from '../../store/game/store'
 import CategoryPicker from './CategoryPicker'
 import Difficultypicker from './DifficultyPicker'
 
 function Wizzard() {
+	const { isSignedIn } = useAuth()
 	const { setIsGameActive } = useGameStore()
 
 	const handleQuitGame = () => {
@@ -24,17 +26,19 @@ function Wizzard() {
 			</section>
 			<section className='flex w-full flex-col items-center justify-center gap-2 p-2 md:flex-row'>
 				<button
-					className='w-full cursor-pointer rounded-md border-none bg-emerald-400/50 px-4 py-3 text-lg font-normal tracking-tight text-green-300 transition-all select-none hover:scale-102 hover:border-white hover:bg-emerald-400 hover:font-medium hover:text-green-800 hover:shadow-xl focus:bg-emerald-400 md:text-xl lg:max-w-xl'
+					className='w-full cursor-pointer rounded-md border-none bg-emerald-400/50 px-4 py-3 text-lg font-normal tracking-tight text-green-300 transition-all select-none hover:scale-102 hover:border-white hover:bg-emerald-400 hover:font-medium hover:text-green-800 hover:shadow-xl focus:bg-emerald-400 md:text-xl'
 					onClick={handleStartGame}
 				>
 					Start game
 				</button>
-				<button
-					className='w-full cursor-pointer rounded-md border-none bg-rose-400/50 px-4 py-3 text-lg font-normal tracking-tight text-red-300 transition-all select-none hover:scale-102 hover:border-white hover:bg-rose-400 hover:font-medium hover:text-red-800 hover:shadow-xl focus:bg-rose-400 md:text-xl lg:max-w-xl'
-					onClick={handleQuitGame}
-				>
-					Quit game
-				</button>
+				{isSignedIn && (
+					<button
+						className='w-full cursor-pointer rounded-md border-none bg-rose-400/50 px-4 py-3 text-lg font-normal tracking-tight text-red-300 transition-all select-none hover:scale-102 hover:border-white hover:bg-rose-400 hover:font-medium hover:text-red-800 hover:shadow-xl focus:bg-rose-400 md:text-xl lg:max-w-xl'
+						onClick={handleQuitGame}
+					>
+						Quit game
+					</button>
+				)}
 			</section>
 		</div>
 	)
