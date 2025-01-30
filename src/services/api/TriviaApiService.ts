5004f5a1d9405bc41981321215eca924a003b9bc
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-30 13:58:09 +0100

    fix: node path

---
+++ src/services/api/TriviaApiService.ts
@@ -0,0 +1,2 @@
+// const BASE_URL = 'https://the-trivia-api.com/v2/questions'
+export function GetQuestions() {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
