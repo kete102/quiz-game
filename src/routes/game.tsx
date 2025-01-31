6ff979e4778d61839fd928d589c00324f01c759a
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-31 13:54:13 +0100

    feat: fix padding and added Header and Footer components

--- src/routes/game.tsx
+++ src/routes/game.tsx
@@ -1,4 +1,6 @@
+import Footer from '@/components/Footer'
 import Game from '@/components/game/Game'
+import Header from '@/components/Header'
 import { useGameStore } from '@/store/game/store'
 import { useUser } from '@clerk/clerk-react'
 import { createFileRoute } from '@tanstack/react-router'
@@ -18,8 +20,10 @@ function RouteComponent() {
 	}, [resetState])
 
 	return (
-		<div className='flex h-full w-full flex-col items-center justify-start'>
+		<div className='flex h-screen w-full flex-col items-center justify-between p-2'>
+			<Header />
 			<Game />
+			<Footer />
 		</div>
 	)
 }
