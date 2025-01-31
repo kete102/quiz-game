3b6c1449821a86b214278dc60d3076be430dbd75
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-31 12:53:45 +0100

    feat: not found page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import NotFound from '@/components/NotFound'
 
 export const Route = createRootRoute({
 	component: RootComponent,
+	notFoundComponent: NotFound,
 })
 
 function RootComponent() {
