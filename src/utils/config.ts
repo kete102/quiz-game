32a2e6039a707aef0ada807d174d648fac78f77d
Neacsu Flavius Catalin <kete102@gmail.com>
2025-01-31 17:42:25 +0100

    fix: env variables

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,3 @@
-import { z } from 'zod'
-
 const env = import.meta.env
 
-const EnvSchema = z.object({
-	VITE_CLERK_PUBLISHABLE_KEY: z.string(),
-})
-
-const processEnv = EnvSchema.parse(env)
-
-export const VITE_CLERK_PUBLISHABLE_KEY = processEnv.VITE_CLERK_PUBLISHABLE_KEY
+export const VITE_CLERK_PUBLISHABLE_KEY = env.VITE_CLERK_PUBLISHABLE_KEY
