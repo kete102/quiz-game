914f6b621ccacfc1ccd3f5d20e2052d760732dde
Neacsu Flavius Catalin <kete102@gmail.com>
2025-02-04 00:40:25 +0100

    feat: isGameActive and endGame values

--- src/store/game/store.ts
+++ src/store/game/store.ts
@@ -16,12 +16,14 @@ interface State {
 	questions: Question[]
 	userAnswers: { questionId: string; isCorrect: boolean }[]
 	currentQuestionIndex: number
+	isGameActive: boolean
 }
 
 interface Actions {
 	startGame: (questions: Question[]) => void
 	selectAnswer: (questionId: string, isCorrect: boolean) => void
 	nextQuestion: () => void
+	endGame: () => void
 	resetGame: () => void
 }
 
@@ -29,6 +31,7 @@ const initialState: State = {
 	questions: [],
 	userAnswers: [],
 	currentQuestionIndex: 0,
+	isGameActive: false,
 }
 
 export const useGameStore = create<State & Actions>((set) => ({
@@ -36,12 +39,24 @@ export const useGameStore = create<State & Actions>((set) => ({
 
 	startGame: (newQuestions: Question[]) => {
 		console.log('State: ', newQuestions)
-		set({ questions: newQuestions, currentQuestionIndex: 0, userAnswers: [] })
+		set({
+			questions: newQuestions,
+			currentQuestionIndex: 0,
+			userAnswers: [],
+			isGameActive: true,
+		})
 	},
+	endGame: () => set({ isGameActive: false }),
 	nextQuestion: () =>
-		set((state) => ({ currentQuestionIndex: state.currentQuestionIndex + 1 })),
+		set((state) => ({
+			currentQuestionIndex: state.currentQuestionIndex + 1,
+		})),
 	selectAnswer: (answers) =>
 		set((state) => ({ ...state, selectedAnswers: answers })),
 	resetGame: () =>
-		set({ questions: [], currentQuestionIndex: 0, userAnswers: [] }),
+		set({
+			questions: [],
+			currentQuestionIndex: 0,
+			userAnswers: [],
+		}),
 }))
