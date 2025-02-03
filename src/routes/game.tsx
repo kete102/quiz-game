b76dbb1aea4a99ae1df04d02a92dc4dc2e368278
Neacsu Flavius Catalin <kete102@gmail.com>
2025-02-04 00:03:24 +0100

    refactor: implement FetchQuestion on beforeLoad root function

--- src/routes/game.tsx
+++ src/routes/game.tsx
@@ -1,28 +1,51 @@
 import Footer from '@/components/Footer'
 import Game from '@/components/game/Game'
 import Header from '@/components/Header'
+import { FetchQuestions } from '@/services/game/questions/Questions'
+import {
+	CategoriesApiValueSchema,
+	DifficultiesApiValueSchema,
+} from '@/services/game/questions/types'
 import { useGameStore } from '@/store/game/store'
-import { useUser } from '@clerk/clerk-react'
 import { createFileRoute } from '@tanstack/react-router'
 import { useEffect } from 'react'
 
 export const Route = createFileRoute('/game')({
+	validateSearch: ({
+		categories,
+		difficulty,
+	}: {
+		categories: CategoriesApiValueSchema[]
+		difficulty: DifficultiesApiValueSchema
+	}) => ({
+		categories,
+		difficulty,
+	}),
+	beforeLoad: async ({ search }) => {
+		const questions = await FetchQuestions({
+			categories: search.categories as CategoriesApiValueSchema[],
+			difficulty: search.difficulty as DifficultiesApiValueSchema,
+		})
+
+		if (!questions) throw new Error('Failed to fetch questions')
+
+		return { questions }
+	},
 	component: RouteComponent,
 })
 
 function RouteComponent() {
-	const { isSignedIn } = useUser()
-	console.log('Game Page: ', isSignedIn)
-	const { resetState } = useGameStore()
+	const { questions } = Route.useRouteContext()
+	const { resetGame } = useGameStore()
 
 	useEffect(() => {
-		return () => resetState()
-	}, [resetState])
+		return () => resetGame()
+	}, [resetGame])
 
 	return (
 		<div className='flex h-screen w-full flex-col items-center justify-between p-2 md:gap-y-10'>
 			<Header />
-			<Game />
+			<Game questions={questions} />
 			<Footer />
 		</div>
 	)
