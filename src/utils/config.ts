7d1b717cfc8c06d2541482352d8958ea452b098f
Neacsu Flavius Catalin <kete102@gmail.com>
2025-02-05 23:00:25 +0100

    core: supabase config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,5 @@
 export const VITE_CLERK_PUBLISHABLE_KEY = import.meta.env
 	.VITE_CLERK_PUBLISHABLE_KEY
+
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
