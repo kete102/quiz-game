dd8390d931d58dbe18359ad7db3a4f72a85c63c7
Neacsu Flavius Catalin <kete102@gmail.com>
2025-02-05 23:52:16 +0100

    feat: supabase
    Get and update Stats for user
    Types from supabase database

---
+++ src/services/game/stats/GameStats.ts
@@ -0,0 +1,127 @@
+import { supabase } from '@/db'
+import { ApiResponse, DBUserStats, UserStats } from './types/types'
+import { PostgrestError } from '@supabase/supabase-js'
+
+export function GameStats() {
+	const getUserStats = async ({
+		userId,
+	}: {
+		userId: string
+	}): Promise<ApiResponse<UserStats>> => {
+		try {
+			const { data, error } = await supabase
+				.from('user_stats')
+				.select('')
+				.eq('user_id', userId)
+				.single<DBUserStats>()
+
+			if (error) {
+				return {
+					success: false,
+					message: 'Error fetching user stats',
+					error: error.message,
+				}
+			}
+
+			const transformedData: UserStats = {
+				userId: data.user_id,
+				totalGames: data.total_games,
+				winRatio: data.win_ratio,
+				correctAnswers: data.correct_answers,
+				wrongAnswers: data.wrong_answers,
+				streak: data.streak,
+				bestStreak: data.best_streak,
+				categoriesPlayed: data.categories_played,
+				lastPlayed: data.last_played_at,
+			}
+
+			return {
+				success: true,
+				message: 'User stats fetched successfully',
+				data: transformedData,
+			}
+		} catch (error) {
+			if (error instanceof PostgrestError) {
+				return {
+					success: false,
+					message: 'DB error',
+					error: error.message,
+				}
+			}
+			return {
+				success: false,
+				message: 'Unexpected error ocurred',
+				error: (error as Error).message,
+			}
+		}
+	}
+
+	const updateUserStats = async ({
+		newStats,
+	}: {
+		newStats: UserStats
+	}): Promise<ApiResponse<UserStats>> => {
+		try {
+			const { data, error } = await supabase
+				.from('user_stats')
+				.update({
+					best_streak: newStats.bestStreak,
+					total_games: newStats.totalGames,
+					streak: newStats.streak,
+					categories_played: newStats.categoriesPlayed,
+					correct_answers: newStats.correctAnswers,
+					wrong_answers: newStats.wrongAnswers,
+					win_ratio:
+						newStats.correctAnswers /
+						(newStats.correctAnswers + newStats.wrongAnswers),
+					last_played_at: newStats.lastPlayed,
+				})
+				.eq('user_id', newStats.userId)
+				.single<DBUserStats>()
+
+			if (error) {
+				return {
+					success: false,
+					message: 'Error updating user stats',
+					error: error.message,
+				}
+			}
+
+			const transformedData: UserStats = {
+				userId: data.user_id,
+				totalGames: data.total_games,
+				winRatio: data.win_ratio,
+				correctAnswers: data.correct_answers,
+				wrongAnswers: data.wrong_answers,
+				streak: data.streak,
+				bestStreak: data.best_streak,
+				categoriesPlayed: data.categories_played,
+				lastPlayed: data.last_played_at,
+			}
+
+			return {
+				success: true,
+				message: 'User stats updated successfully',
+				data: transformedData,
+			}
+		} catch (error) {
+			if (error instanceof PostgrestError) {
+				return {
+					success: false,
+					message: 'DB error',
+					error: error.message,
+				}
+			}
+			return {
+				success: false,
+				message: 'Unexpected error ocurred',
+				error: (error as Error).message,
+			}
+		}
+	}
+
+	return {
+		getUserStats,
+		updateUserStats,
+	}
+}
---
+++ src/services/game/stats/types/types.ts
@@ -0,0 +1,29 @@
+import { Database } from './supabase'
+
+export interface UserStats {
+	userId: string
+	correctAnswers: number
+	wrongAnswers: number
+	categoriesPlayed: string[]
+	winRatio: number
+	streak: number
+	bestStreak: number
+	lastPlayed: string
+	totalGames: number
+}
+
+export type DBUserStats = Database['public']['Tables']['user_stats']['Row']
+
+interface SuccessResponse<T> {
+	success: true
+	message: string
+	data: T
+}
+
+interface ErrorResponse {
+	success: false
+	message: string
+	error: string
+}
+
+export type ApiResponse<T> = SuccessResponse<T> | ErrorResponse
