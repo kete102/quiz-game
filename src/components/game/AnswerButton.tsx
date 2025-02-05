382d705d38c6c3844367e445203749b1ad4e4919
Neacsu Flavius Catalin <kete102@gmail.com>
2025-02-05 21:28:17 +0100

    styles: adjust text and bg colors

--- src/components/game/AnswerButton.tsx
+++ src/components/game/AnswerButton.tsx
@@ -28,10 +28,10 @@ function AnswerButton({
 			disabled={disabled}
 			onClick={() => handleSelectAnswer(answer)}
 			className={clsx(
-				`inline-flex w-full transform cursor-pointer items-center gap-x-2 rounded-md border-[2px] border-gray-950/50 p-2 pl-3 text-white transition-all duration-200 hover:scale-105 active:scale-97`,
+				`inline-flex w-full transform cursor-pointer items-center justify-center gap-x-2 rounded-md bg-gray-400/50 p-3 text-xl font-medium text-white italic transition-all duration-200 hover:scale-105 active:scale-97`,
 				{
-					'bg-green-600': showAsCorrect,
-					'bg-red-600': showAsIncorrect,
+					'bg-green-600 text-green-200 opacity-100': showAsCorrect,
+					'bg-red-600 text-red-300 opacity-80': showAsIncorrect,
 					'ring-2 ring-blue-400': !showResult && isSelected,
 					'cursor-not-allowed opacity-50': disabled,
 				}
