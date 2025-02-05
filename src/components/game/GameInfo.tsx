7cc47083cc958a681d59b138f8c02d0d2031fc91
Neacsu Flavius Catalin <kete102@gmail.com>
2025-02-05 21:10:17 +0100

    refactor: added the score form game store

--- src/components/game/GameInfo.tsx
+++ src/components/game/GameInfo.tsx
@@ -1,30 +1,22 @@
 import { useGameStore } from '@/store/game/store'
-import { ListOrdered, ThumbsUp, ThumbsDown } from 'lucide-react'
+import { ListOrdered, Trophy } from 'lucide-react'
 
 interface Props {
 	currentQuestionIndex: number
 }
 function GameInfo({ currentQuestionIndex }: Props) {
-	const { userAnswers, questions } = useGameStore()
+	const { questions, score } = useGameStore()
 
 	return (
-		<section className='flex w-full flex-col items-center px-4 text-white md:max-w-2xl lg:max-w-3xl'>
-			<div className='flex w-full items-center justify-center gap-x-3'>
-				<h3 className='inline-flex items-center gap-x-2 rounded-md bg-black/50 p-3 text-2xl font-semibold transition-all hover:scale-103'>
-					<ListOrdered className='size-6' />
-					{currentQuestionIndex}/{questions.length}
-				</h3>
-				<p className='flex items-center gap-x-3 text-2xl font-medium'>
-					<span className='inline-flex items-center gap-x-2 rounded-md bg-green-800 p-3 transition-all hover:scale-103'>
-						<ThumbsUp className='size-5' />
-						{userAnswers.length}
-					</span>
-					<span className='inline-flex items-center gap-x-2 rounded-md bg-red-800 p-3 transition-all hover:scale-103'>
-						<ThumbsDown className='size-5' />
-						{userAnswers.length}
-					</span>
-				</p>
-			</div>
+		<section className='flex w-full flex-row items-center justify-center gap-x-4 px-4 text-white select-none md:max-w-2xl lg:max-w-3xl'>
+			<span className='inline-flex items-center gap-x-2 rounded-md bg-white/5 p-3 text-2xl font-normal shadow-sm shadow-white/20 transition-transform duration-200 hover:scale-103'>
+				<Trophy className='size-6' />
+				{score}/100
+			</span>
+			<span className='inline-flex items-center gap-x-2 rounded-md bg-black/40 p-3 text-2xl font-normal shadow-sm shadow-black/50 transition-transform duration-200 hover:scale-103'>
+				<ListOrdered className='size-6' />
+				{currentQuestionIndex + 1}/{questions.length}
+			</span>
 		</section>
 	)
 }
