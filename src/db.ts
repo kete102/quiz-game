6d377440d9eeee558ca1490835c05f163151f5f5
Neacsu Flavius Catalin <kete102@gmail.com>
2025-02-07 18:39:33 +0100

    fix: supabase and clerk client

--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,22 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from './services/game/stats/types/supabase'
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './utils/config'
+import { SUPABASE_URL, SUPABASE_KEY } from './utils/config'
 import { ActiveSessionResource } from '@clerk/types'
 
 export function CreateSupabaseClerkClient(
 	session: ActiveSessionResource | null | undefined
 ) {
-	return createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
+	return createClient<Database>(SUPABASE_URL!, SUPABASE_KEY!, {
 		global: {
 			fetch: async (url, options = {}) => {
 				const clerkToken = await session?.getToken({
 					template: 'supabase',
 				})
+
+				if (!clerkToken) {
+					console.log('Error Clerk token')
+					throw new Error('Failed to get Clerk token')
+				}
 
 				const headers = new Headers(options?.headers)
 				headers.set('Authorization', `Bearer ${clerkToken}`)
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,4 +2,4 @@ export const VITE_CLERK_PUBLISHABLE_KEY = import.meta.env
 	.VITE_CLERK_PUBLISHABLE_KEY
 
 export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
