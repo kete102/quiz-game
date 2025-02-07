df136e03cfb70c7d9241184e8caf595fead48f9e
Neacsu Flavius Catalin <kete102@gmail.com>
2025-02-07 22:13:24 +0100

    feat: custom hook useUserStats and supabaseClerkClient

--- src/db.ts
+++ src/db.ts
@@ -3,28 +3,20 @@ import { Database } from './services/game/stats/types/supabase'
 import { SUPABASE_URL, SUPABASE_KEY } from './utils/config'
 import { ActiveSessionResource } from '@clerk/types'
 
-export function CreateSupabaseClerkClient(
+export const CreateSupabaseClerkClient = (
 	session: ActiveSessionResource | null | undefined
-) {
+) => {
 	return createClient<Database>(SUPABASE_URL!, SUPABASE_KEY!, {
 		global: {
 			fetch: async (url, options = {}) => {
-				const clerkToken = await session?.getToken({
-					template: 'supabase',
-				})
+				const clerkToken = await session?.getToken({ template: 'supabase' })
 
-				if (!clerkToken) {
-					console.log('Error Clerk token')
-					throw new Error('Failed to get Clerk token')
-				}
+				if (!clerkToken) throw new Error('Failed to get Clerk token')
 
 				const headers = new Headers(options?.headers)
 				headers.set('Authorization', `Bearer ${clerkToken}`)
 
-				return fetch(url, {
-					...options,
-					headers,
-				})
+				return fetch(url, { ...options, headers })
 			},
 		},
 	})
--- src/hooks/game/useUserStats.ts
+++ src/hooks/game/useUserStats.ts
@@ -1,20 +1,41 @@
 import { CreateSupabaseClerkClient } from '@/db'
-import GameStatsService from '@/services/game/stats/GameStats'
+import GameStatsService from '@/services/game/stats/GameStatsService'
 import { useSession } from '@clerk/clerk-react'
+import { useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
 
 export function useUserStats() {
 	const { session } = useSession()
-	const supabaseClerkClient = CreateSupabaseClerkClient(session)
+	const supabaseClerkClient = useMemo(() => {
+		if (!session) return null
+		return CreateSupabaseClerkClient(session)
+	}, [session])
 
+	const {
+		data: userStats,
+		isLoading,
+		error,
+		refetch,
+	} = useQuery({
+		queryKey: ['user-stats', session?.user.id],
+		queryFn: async () => {
+			if (!supabaseClerkClient) return
+			const response =
+				await GameStatsService(supabaseClerkClient).getUserStats()
+
+			if (!response.success) throw new Error(response.error)
+
+			return response.data
+		},
+		enabled: !!session, // Only fetches de stats if session exists
+		staleTime: 5 * 60 * 1000, // 5 mins in cache
+	})
 	const updateUserStats = async () => {
-		if (!session) {
-			return
-		}
-
+		if (!session || !supabaseClerkClient) return
 		try {
-			const response = await GameStatsService({
-				supabaseClerkClient,
-			}).updateUserStats({
+			const response = await GameStatsService(
+				supabaseClerkClient
+			).updateUserStats({
 				bestStreak: 3,
 				correctAnswers: 9,
 				streak: 3,
@@ -24,17 +45,24 @@ export function useUserStats() {
 			})
 
 			if (!response.success) {
-				console.log('Error updating users stats: ', response.error)
+				throw new Error(response.error)
 			}
+
 			if (response.success) {
 				console.log('User stats updated: ', response.data)
+				refetch() // Refetches de user stats is the update is succesfull
 			}
+
+			return response.data
 		} catch (error) {
 			console.error(error)
 		}
 	}
 
 	return {
+		userStats,
+		isLoading,
+		error,
 		updateUserStats,
 	}
 }
